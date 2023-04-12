5248fbfd1bfbc24cab4f2c901e78565eed97d373
Nadav Dayan <ndayan@appdev.yearup.org>
2023-04-12 11:32:47 -0400

    Added a shopping list

---
+++ src/main/java/com/learntocode/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.learntocode;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Shopping List");
+        System.out.println(" - Eggs");
+        System.out.println(" - Bacon");
+        System.out.println(" - Milk");
+        System.out.println(" - Bananas");
+        System.out.println(" - Oat Milk");
+        System.out.println(" - Lettuce");
+        System.out.println(" - Chicken drumsticks");
+        System.out.println(" - Rye Bread");
+        System.out.println(" - Paper Towels");
+        System.out.println(" - Plastic Plates");
+    }
+}
